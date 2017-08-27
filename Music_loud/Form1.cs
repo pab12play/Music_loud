3b46425b79d762b9a444f4b21c3ab7ab8100a9dc
Pablo Pineda <pablo12.izq@gmail.com>
2017-08-27 14:44:05 -0600

    Added WMP Library to project, and playback support

--- Music_loud/Form1.cs
+++ Music_loud/Form1.cs
@@ -7,19 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WMPLib;
 
 namespace Music_loud
 {
     public partial class Form1 : Form
     {
+        WindowsMediaPlayer player;
         public Form1()
         {
             InitializeComponent();
+            player = new WindowsMediaPlayer();
+            player.URL = "test.mp3";
+            player.controls.stop();
         }
 
         private void button_play_Click(object sender, EventArgs e)
         {
-
+            player.controls.play();
         }
     }
 }
