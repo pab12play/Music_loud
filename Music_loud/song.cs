79787f209f8375e399db870042fa324b36c9c90f
Pablo Pineda <pablo12.izq@gmail.com>
2017-08-28 12:00:55 -0600

    Changed atribute 'lenght' from class 'song', int -> TimeSpan

--- Music_loud/song.cs
+++ Music_loud/song.cs
@@ -9,13 +9,13 @@ namespace Music_loud
     class song
     {
         private string title;
-        private int lenght;
+        private TimeSpan lenght;
         private string author;
         private string album;
         private string path;
 
 
-        public song(string title, int lenght, string author, string album)
+        public song(string title, TimeSpan lenght, string author, string album)
         {
             this.title = title;
             this.lenght = lenght;
@@ -24,7 +24,7 @@ namespace Music_loud
         }
 
         public string Title { get => title; set => title = value; }
-        public int Lenght { get => lenght; set => lenght = value; }
+        public TimeSpan Lenght { get => lenght; set => lenght = value; }
         public string Author { get => author; set => author = value; }
         public string Album { get => album; set => album = value; }
         public string Path { get => path; set => path = value; }
